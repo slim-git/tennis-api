15c6af77e1114e7e671b9106353c448c5846f168
sghorbal <s.ghorbal@groupe-reussite.fr>
2025-02-06 13:32:29 +0100

    Initial commit

---
+++ src/sql.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import psycopg2
+from typing import Literal
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_HOST = os.getenv("PG_HOST")
+PG_PORT = os.getenv("PG_PORT")
+PG_DB = os.getenv("PG_DB")
+PG_SSLMODE = os.getenv("PG_SSLMODE")
+
+def _get_connection() -> psycopg2.extensions.connection:
+    """
+    Get a connection to the Postgres database
+    """
+    conn = psycopg2.connect(
+        dbname=PG_DB,
+        user=PG_USER,
+        password=PG_PASSWORD,
+        host=PG_HOST,
+        port=PG_PORT,
+        sslmode=PG_SSLMODE,
+    )
+    return conn
+
+def load_matches_from_postgres(
+        table_name: Literal['atp_data', 'wta_data'],
+        from_date: str = None,
+        to_date: str = None) -> pd.DataFrame:
+    """
+    Load data from Postgres
+    """
+    if not to_date:
+        to_date = datetime.now().strftime("%Y-%m-%d")
+    
+    if not from_date:
+        from_date = "1900-01-01"
+    
+    query = f"SELECT * FROM {table_name} WHERE date BETWEEN %s AND %s"
+    vars = [from_date, to_date]
+    
+    with _get_connection() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query, vars)
+            data = cursor.fetchall()
+
+    data = pd.DataFrame(data, columns=[desc[0] for desc in cursor.description])
+
+    return data
