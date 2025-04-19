650da2e827b3f57d302e94929fe10cb41bf55560
sghorbal <s.ghorbal@groupe-reussite.fr>
2025-04-19 17:57:56 +0200

    start rq scheeduler and worker if it makes sense

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+# Launch rqscheduler --url $REDIS_URL and rq worker in the background
+# if REDIS_URL is set and ENV is not test
+if [[ -n "$REDIS_URL" && "$ENV" != "test" ]]; then
+    echo "Starting rq scheduler and worker..."
+    rq worker --url $REDIS_URL &
+    rqscheduler --url $REDIS_URL &
+else
+    echo "Skipping rq scheduler and worker startup."
+fi
+
 # Run the API
 uvicorn src.main:app --host 0.0.0.0 --port 7860
