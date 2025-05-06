a8be403349998497148ee534ca0021bc526572fa
sghorbal <s.ghorbal@groupe-reussite.fr>
2025-05-06 10:56:39 +0200

    use a pool of workers instead of a single one

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Launch rqscheduler --url $REDIS_URL and rq worker in the background
+# Launch rqscheduler --url $REDIS_URL and rq workers in the background
 # if REDIS_URL is set and ENV is not test
 if [[ -n "$REDIS_URL" && "$ENV" != "test" ]]; then
-    echo "Starting rq scheduler and worker..."
-    rq worker --url $REDIS_URL &
+    echo "Starting rq scheduler and workers..."
+    rq worker-pool -n ${NB_RQ_WORKERS:-1} --url $REDIS_URL &
     rqscheduler --url $REDIS_URL &
 else
-    echo "Skipping rq scheduler and worker startup."
+    echo "Skipping rq scheduler and workers startup."
 fi
 
 # Run the API
