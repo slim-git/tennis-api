52e0bc6bcbbed123519b728da027944dd8d86a15
sghorbal <s.ghorbal@groupe-reussite.fr>
2025-05-06 11:27:58 +0200

    improve deduplication of jobs

--- src/jobs/player.py
+++ src/jobs/player.py
@@ -1,9 +1,11 @@
 import logging
 from datetime import datetime
 from rq_scheduler import Scheduler
+from rq.job import Job
+
+from src.jobs.scheduler import get_redis_connection, is_job_scheduled_queued_or_started
 from src.repository.common import get_session
 from src.service import player
-from src.jobs.scheduler import get_redis_connection
 
 logging.basicConfig(level=logging.INFO,
                     handlers=[logging.StreamHandler()])
@@ -16,23 +18,25 @@ def fill_player_details(raw_player_name: str):
     except Exception as e:
         logger.error(f"Error: {e}")
 
-def schedule_player_details(raw_player_name: str):
+def schedule_player_details(raw_player_name: str) -> Job:
     """
     Schedule the job to fill player details
     """
+    job_id = f'player_details_{raw_player_name}'
+
+    if job := is_job_scheduled_queued_or_started(job_id=job_id):
+        return job
+    
     with get_redis_connection() as r:
         scheduler = Scheduler(connection=r)
-        # Check if the job is already scheduled
-        for job in scheduler.get_jobs():
-            if job.id == raw_player_name:
-                logger.debug(f"Job for player {raw_player_name} already scheduled.")
-                return
 
         # Schedule the job to run immediately
         logger.info(f"Scheduling job for player: {raw_player_name}")
-        scheduler.schedule(
+        job = scheduler.schedule(
             scheduled_time=datetime.utcnow(),
             func=fill_player_details,
             kwargs={'raw_player_name': raw_player_name},
-            id=raw_player_name, # Ensures deduplication of jobs
+            id=job_id, # Ensures deduplication of jobs
         )
+
+        return job
